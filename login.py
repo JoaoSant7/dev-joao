14b1c0867ab0f483e594c0abc1eaeb34f7825c76
JoaoSant7 <pedro_romano7@proton.me>
2025-08-15 20:10:16 -0300

    commit login

---
+++ login.py
@@ -0,0 +1,10 @@
+usuario_registrado = "admin"
+senha_registrada = "WinRad10"
+
+usuario = input("Digite seu nome de usuário: ")
+senha = input("Digite sua senha: ")
+
+if usuario == usuario_registrado and senha == senha_registrada:
+    print("Acesso concedido!")
+else:
+    print("Acesso negado!")
