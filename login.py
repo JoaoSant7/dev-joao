1dd34aa6533c3d8a56b50d249c165b9a563f6f45
Pedro Santana <pedro_romano7@proton.me>
2025-08-15 20:14:16 -0300

    fetch

--- login.py
+++ login.py
@@ -4,7 +4,7 @@ senha_registrada = "WinRad10"
 usuario = input("Digite seu nome de usuário: ")
 senha = input("Digite sua senha: ")
 
-if usuario == usuario_registrado and senha == senha_registrada:
-    print("Acesso concedido!")
+if usuario == usuario_registrado and senha == senha_registrada: 
+    print("Acesso concedido!") 
 else:
     print("Acesso negado!")
